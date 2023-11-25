f79c89a9eedc282ba016e915aa8af6de5491baeb
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-11-25 11:20:46 +0530

    SignUp & Login user route+controllers

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+// Connect to DB
 connectDB();
+
+// Middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
