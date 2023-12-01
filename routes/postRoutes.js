49378118cca9d9b5428071b0356580a5014bc4ff
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-12-01 22:54:27 +0530

    Post retrieval endpoints

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,12 +4,16 @@ import {
   createPost,
   deletePost,
   getAllPosts,
+  getPostById,
+  getPostsByUserId,
   updatePost,
 } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.get("/", getAllPosts);
+router.get("/user/:userId", protectRoute, getPostsByUserId);
+router.get("/:postId", getPostById);
 router.post("/create", protectRoute, createPost);
 router.patch("/update/:postId", protectRoute, updatePost);
 router.delete("/:postId", protectRoute, deletePost);
